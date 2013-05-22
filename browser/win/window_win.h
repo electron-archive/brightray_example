91cbd3d68c33db73af93a9e4da6271cd46d767e9
Adam Roben <adam@roben.org>
2013-05-22 16:59:47 -0400

    Show a window on Windows
    You can't show the dev tools, but other than that things seem to work!
    
    * vendor/brightray 76867b9...fa15d36 (2):
      > Get libchromiumcontent back on master
      > Stub out InspectableWebContentsViewWin

--- browser/win/window_win.h
+++ browser/win/window_win.h
@@ -2,6 +2,7 @@
 #define BRIGHTRAY_EXAMPLE_BROWSER_WIN_WINDOW_WIN_H_
 
 #include "browser/window.h"
+#include <Windows.h>
 
 namespace brightray_example {
 
@@ -13,6 +14,14 @@ class WindowWin : public Window {
   virtual void Show() OVERRIDE;
 
  private:
+  static HINSTANCE instance_handle_;
+  static void RegisterWindowClass();
+  static LRESULT CALLBACK WndProc(HWND, UINT message, WPARAM, LPARAM);
+
+  void ResizeWebContents();
+
+  HWND window_;
+
   DISALLOW_COPY_AND_ASSIGN(WindowWin);
 };
 
