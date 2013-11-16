3fc5a5a8b01af6c403e8771d484972ca0dcc2a89
Patrick Reynolds <dukepiki@gmail.com>
2013-11-16 01:36:14 -0600

    exit cleanly when the window is closed

--- browser/linux/window_linux.cc
+++ browser/linux/window_linux.cc
@@ -1,6 +1,7 @@
 #include <gtk/gtk.h>
 #include "window_linux.h"
 
+#include "base/message_loop/message_loop.h"
 #include "brightray/browser/inspectable_web_contents.h"
 #include "brightray/browser/inspectable_web_contents_view.h"
 
@@ -10,10 +11,10 @@ Window* Window::Create(brightray::BrowserContext* browser_context) {
   return new WindowLinux(browser_context);
 }
 
-static void window_closed() {
+static void window_closed(GtkWindow *gtk_window, WindowLinux *window_object) {
   // There's only one window, so exit when it closes.
-  // `gtk_main_quit` would be cleaner here, but it doesn't seem to work.
-  exit(0);
+  delete window_object;
+  base::MessageLoop::current()->QuitWhenIdle();
 }
 
 WindowLinux::WindowLinux(brightray::BrowserContext* browser_context)
@@ -21,7 +22,7 @@ WindowLinux::WindowLinux(brightray::BrowserContext* browser_context)
   window_ = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title(GTK_WINDOW(window_), "Brightray Example");
   gtk_container_border_width(GTK_CONTAINER(window_), 8);
-  gtk_signal_connect(GTK_OBJECT(window_), "destroy", GTK_SIGNAL_FUNC(window_closed), 0);
+  g_signal_connect(GTK_OBJECT(window_), "destroy", G_CALLBACK(window_closed), 0);
 
   // Make the window 70% the width and height of the default screen.
   GdkScreen *screen = gdk_screen_get_default();
